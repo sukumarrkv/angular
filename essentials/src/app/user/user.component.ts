b7866da2f24f2c2af5c9e685af45a1c575098e7f
sukumarrkv <kypa.sukumar3@gmail.com>
2025-09-02 11:36:07 +0530

    Made user component interactive, added cdd properties, event listeners

--- essentials/src/app/user/user.component.ts
+++ essentials/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { DUMMY_USERS } from "../dummy-users";
+
+const randomNumber = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -6,4 +9,16 @@ import { Component } from "@angular/core";
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {}
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomNumber];
+
+  //This is called getter which gives an result which we can use in othe rplaces for eg in html.
+  get imagePath() {
+    return 'assets/users/'+ this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    const randomNumber = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomNumber];
+  }
+}
