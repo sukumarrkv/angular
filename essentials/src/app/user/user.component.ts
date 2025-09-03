d6c9b4c3cae70ed816bde61e2e562e3b0911b1cc
sukumarrkv <kypa.sukumar3@gmail.com>
2025-09-03 11:36:41 +0530

    Display mutiple users using input decorator and input signals

--- essentials/src/app/user/user.component.ts
+++ essentials/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, computed, input, Input } from "@angular/core";
 import { DUMMY_USERS } from "../dummy-users";
 
 const randomNumber = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,6 +10,8 @@ const randomNumber = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  //Below code is added for practice on how we display user name, image and handle events in Angular
+  /*
   selectedUser = DUMMY_USERS[randomNumber];
 
   //This is called getter which gives an result which we can use in othe rplaces for eg in html.
@@ -21,4 +23,29 @@ export class UserComponent {
     const randomNumber = Math.floor(Math.random() * DUMMY_USERS.length);
     this.selectedUser = DUMMY_USERS[randomNumber];
   }
+    */
+
+  //We are trying to get this value for avatar from another component hence @Input is used, Also ! is speicific to typescript
+  //where in we are telling that the value of avatar is not empty and will be populated with value
+
+  //Below is how we take inputs using Input Decorator
+  @Input({required: true}) avatar!: String;
+  @Input({required: true}) name!: String;
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  //Using signal to take inputs
+  /*
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => {
+    return  'assets/users/' + this.avatar();
+  })
+  */
+
+
+  onSelectUser() {}
 }
