06b29331dbe147ad052dd0c286cab89ee2816a21
sukumarrkv <kypa.sukumar3@gmail.com>
2025-09-25 11:32:14 +0530

    Split app component into multiple components

---
+++ components-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-server-status',
+  standalone: true,
+  templateUrl: './server-status.componenet.html',
+  styleUrl: './server-status.componenet.css'
+})
+export class ServerStatusComponent {
+
+}
---
+++ components-deep-dive/src/app/dashboard/traffic/traffic.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-traffic',
+  standalone: true,
+  templateUrl: './traffic.component.html',
+  styleUrl: './traffic.component.css'
+})
+export class TrafficComponent {
+
+}
