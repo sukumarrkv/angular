1f364a05757f790a4414b99e3e36b97d1e7db46a
sukumarrkv <kypa.sukumar3@gmail.com>
2025-09-29 11:28:56 +0530

    Added new ticket and ticket components and added html for new ticket ccomponent

--- components-deep-dive/src/app/dashboard/tickets/ticket.component.ts
+++ components-deep-dive/src/app/dashboard/tickets/ticket.component.ts
@@ -1,10 +1,12 @@
 import { Component } from "@angular/core";
+import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
   templateUrl:'./ticket.component.html',
-  styleUrl: './ticket.component.css'
+  styleUrl: './ticket.component.css',
+  imports: [NewTicketComponent]
 })
 export class TicketComponent {
   
