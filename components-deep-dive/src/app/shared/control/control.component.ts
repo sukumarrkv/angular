069c716249fad35bd6efd33a73e631c17d53255a
sukumarrkv <kypa.sukumar3@gmail.com>
2025-10-08 11:32:22 +0530

    Implemented host related features in angular

--- components-deep-dive/src/app/shared/control/control.component.ts
+++ components-deep-dive/src/app/shared/control/control.component.ts
@@ -7,7 +7,12 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   //Below setting will apply styles to the html elements projected using ng-content
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  //This adds properties to the host elementsas below
+  //<app-control class="control" />
+  host: {
+    class: 'control'
+  }
 })
 export class ControlComponent {
   label = input.required<string>();
