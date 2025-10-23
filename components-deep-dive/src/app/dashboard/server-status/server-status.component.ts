e2fe786c266d84a8c40168e6e04b153df2362921
sukumarrkv <kypa.sukumar3@gmail.com>
2025-10-23 11:37:12 +0530

    Implemented different types of class binding and style binding

--- components-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ components-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -7,5 +7,24 @@ import { Component } from "@angular/core";
   styleUrl: './server-status.componenet.css'
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  //currentStatus = 'offline';
+  //Angular will allow us to assign any values for currentStatus as it is a string
+  //To restrict for some define values use as below
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  //Dynamically setting current status using random
+
+  ngOnInit() {
+    setInterval(() => {
+      const random = Math.random(); //0 to 0.9999999999999
+
+      if(random < 0.5) {
+        this.currentStatus = 'online';
+      } else if(random < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 3000)
+  }
 }
