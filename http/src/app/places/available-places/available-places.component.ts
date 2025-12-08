2271ed89c83cac0e12929864139d412623796440
sukumarrkv <kypa.sukumar3@gmail.com>
2025-12-08 11:32:02 +0530

    Added get pai to fetch places and handled loading state

--- http/src/app/places/available-places/available-places.component.ts
+++ http/src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +13,31 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetchingPlaces = signal(false);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.isFetchingPlaces.set(true);
+    const getSubscription = this.httpClient.get<{places: Place[]}>("http://localhost:3000/places")
+    .pipe(map((response) => response.places))
+    .subscribe({
+      next: (places) => {
+        //console.log(response.places);
+        this.places.set(places);
+      },
+      error: (error) => {
+        console.log(error.message);
+      },
+      complete: () => {
+        this.isFetchingPlaces.set(false);
+      }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      getSubscription.unsubscribe();
+    })
+  }
 }
