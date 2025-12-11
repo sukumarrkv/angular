e0519c838997506dfbcf366bfb294e0662a662de
sukumarrkv <kypa.sukumar3@gmail.com>
2025-12-11 11:08:47 +0530

    Intercept request and response in angular

--- http/src/main.ts
+++ http/src/main.ts
@@ -1,9 +1,32 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
+function loggingInterceptors(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+  //we can modify the request by using clone method and pass it to next method.
+  // const req = request.clone({
+  //   headers: request.headers.set('X_DEBUG', 'TESTING');
+  // })
+  console.log('[Outgoing request]');
+  console.log(request);
+  //return next(request);
+  return next(request).pipe(
+    tap({
+      next: event => {
+        if(event.type === HttpEventType.Response) {
+          console.log('[Incoming response]');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      }
+    })
+  )
+}
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
+  providers: [provideHttpClient(
+    withInterceptors([loggingInterceptors])
+  )]
 }).catch((err) => console.error(err));
