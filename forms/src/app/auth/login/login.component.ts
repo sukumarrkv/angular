043e3175fe89430d9651ffe6362a7762770a100a
sukumarrkv <kypa.sukumar3@gmail.com>
2025-12-16 11:32:18 +0530

    Worked on changing the login form programatically, worked on signup form

--- forms/src/app/auth/login/login.component.ts
+++ forms/src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { of } from 'rxjs';
+import { debounceTime, of } from 'rxjs';
 
 //creating custom validator
 function mustContainQuestionMark(control: AbstractControl) {
@@ -16,8 +16,7 @@ function mustContainQuestionMark(control: AbstractControl) {
 function isEmailUnique(control: AbstractControl) {
   if(control.value !== "test@gmail.com") { //In ideal world we send a http request here to check for unique
     return of(null); 
-    //we must written observable from async validator. of is such method from rxjs we can use which quickly subscribes and return value.
-
+    //we must return observable from async validator. of is such method from rxjs we can use which quickly subscribes and return value.
   }
 
   return of({isEmailUnique: false});
@@ -30,7 +29,7 @@ function isEmailUnique(control: AbstractControl) {
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   form = new FormGroup({
     email: new FormControl('', {
       validators: [Validators.email, Validators.required],
@@ -40,6 +39,21 @@ export class LoginComponent {
       validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark]
     })
   });
+
+  ngOnInit(): void {
+    const storedEmail = window.localStorage.getItem('email');
+
+    if(storedEmail) {
+      const savedEmail = JSON.parse(storedEmail);
+      this.form.controls.email.setValue(savedEmail.email);
+    }
+
+    this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: value => {
+        window.localStorage.setItem('email', JSON.stringify({email: value.email}))
+      }
+    })
+  }
 
   get isEmailInvalid() {
     return this.form.controls.email.touched && this.form.controls.email.dirty && this.form.controls.email.invalid;
--- forms/src/app/auth/signup/signup.component.ts
+++ forms/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,48 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ɵInternalFormsSharedModule, ReactiveFormsModule, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  signupForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required]
+    }),
+    passwords: new FormGroup({
+      password: new FormControl('', {
+        validators:[Validators.required, Validators.minLength(6)]
+      }),
+      confirmPassword: new FormControl('', {
+        validators:[Validators.required, Validators.minLength(6)]
+      })
+    }),
+    firstName: new FormControl('', {validators: [Validators.required]}),
+    lastName: new FormControl('', {validators: [Validators.required]}),
+    address: new FormGroup({
+      street: new FormControl('', {validators: [Validators.required]}),
+      number: new FormControl('', {validators: [Validators.required]}),
+      postalCode: new FormControl('', {validators: [Validators.required]}),
+      city: new FormControl('', {validators: [Validators.required]}),
+    }),
+    role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', {validators: [Validators.required]}),
+    findUs: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false)
+    ]),
+    agree: new FormControl(false, {validators: [Validators.required]})
+  });
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+  }
+
+  onReset() {
+    this.signupForm.reset();
+  }
+}
